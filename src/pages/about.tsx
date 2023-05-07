3dd8e3133f13020e6a461476b15ccc9dccc3b71d
JayCode Developer <JayCode.contakt@gmail.com>
2023-05-07 22:55:39 +0200

    Created simple navigation

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,24 @@
+import Link from "next/link";
+
 export default function About() {
   return (
     <main>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/experience">Experience</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
       <h1>About</h1>
     </main>
   );
