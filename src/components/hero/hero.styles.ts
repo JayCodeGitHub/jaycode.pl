55d58c45db17e1aa6077a980ac666767fe394310
JayCode Developer <JayCode.contakt@gmail.com>
2023-05-09 17:52:19 +0200

    Added content to hero

--- src/components/hero/hero.styles.ts
+++ src/components/hero/hero.styles.ts
@@ -1,5 +1,22 @@
 import styled from "styled-components";
+import Image from "next/image";
 
 export const StyledWrapper = styled.header`
     height: 100vh;
+`
+export const StyledTitle = styled.h1`
+`
+export const StyledSubTitle = styled.h2`
+`
+
+export const StyledImage = styled(Image)`
+    width: 90vw;
+    height: 40vw;
+    object-fit: cover;
+    object-position: top;
+    border-radius: 1.2rem;
+    @media (min-width: 1024px) {
+        width: 19.4rem;
+        height: 25rem;
+    }
 `
--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,13 +1,41 @@
-import { StyledWrapper } from "./hero.styles";
+import {
+  StyledWrapper,
+  StyledTitle,
+  StyledSubTitle,
+  StyledImage,
+} from "./hero.styles";
 import Button from "../button/button.styles";
+import Link from "next/link";
+
+const heroItems = {
+  title: `Hi, I'm Jakub`,
+  subTitle: `A passionate Front-end Developer`,
+  button: `Download my CV`,
+  buttonHref: `https://drive.google.com/file/d/1X1U-XW9mIOJ3B3jUPhQrzKB6FKEXApam/view?usp=sharing`,
+  secondButton: `Let's talk`,
+  secondButtonHref: `/contact`,
+};
 
 export default function Hero() {
   return (
     <StyledWrapper>
-      <h1>Hero</h1>
+      <StyledTitle>{heroItems.title}</StyledTitle>
+      <StyledSubTitle>{heroItems.subTitle}</StyledSubTitle>
       <a href="#projects">
-        <Button>See More</Button>
+        <button>See More</button>
       </a>
+      <a href={heroItems.buttonHref} target="_blank">
+        <Button>{heroItems.button}</Button>
+      </a>
+      <Link href={heroItems.secondButtonHref}>
+        <Button>{heroItems.secondButton}</Button>
+      </Link>
+      <StyledImage
+        src="/images/profile.jpg"
+        width={1000}
+        height={615}
+        alt="Profile image"
+      />
     </StyledWrapper>
   );
 }
