ed0ea53dacee8767d389841f12d1cf4e5384bd63
JayCode Developer <JayCode.contakt@gmail.com>
2023-05-09 17:35:23 +0200

    Styled button

--- src/components/button/button.styles.ts
+++ src/components/button/button.styles.ts
@@ -1,7 +1,27 @@
 import styled from 'styled-components'
 
-const Button = styled.button`
-
+export const Button = styled.button`
+    font-size: ${({ theme }) => theme.font.size.xs};
+    background-color: ${({ theme }) => theme.white};
+    color: ${({ theme }) => theme.black};
+    border: solid 3px ${({ theme }) => theme.white};
+    font-weight: 500;
+    display: flex; 
+    justify-content: center;
+    align-items: center;
+    border-radius: 1.2rem;
+    padding: 1.5rem 2.5rem;
+    transition: color, background-color 0.2s ease-in-out;
+    cursor: pointer;
+    ${({ theme }) => theme.mq.desktop}  {
+        font-size: ${({ theme }) => theme.font.size.s};
+        margin: 0 20px;
+        padding: 2rem 4.5rem;
+    }
+    &:hover {
+        background: transparent;
+        color: ${({ theme }) => theme.white};
+    }
 `
 
 export default Button;
