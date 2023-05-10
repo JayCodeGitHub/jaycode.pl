2ff2ae98d9d88889302f170607d538d967976e8f
JayCode Developer <JayCode.contakt@gmail.com>
2023-05-10 15:05:41 +0200

    Created contact Items

---
+++ src/components/contactItems/contactItems.tsx
@@ -0,0 +1,9 @@
+import { StyledWrapper } from "./contactItems.styles.";
+
+export default function ContactItems() {
+  return (
+    <StyledWrapper>
+      <h1>ContactItems</h1>
+    </StyledWrapper>
+  );
+}
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/header/header";
+import ContactItems from "@/components/contactItems/contactItems";
 
 export default function Contact() {
   return (
     <main>
       <Header title="Contact" />
+      <ContactItems />
     </main>
   );
 }
