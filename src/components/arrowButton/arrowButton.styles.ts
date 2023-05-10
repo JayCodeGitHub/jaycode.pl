e27e862349ed20d55ece02f9578eeab338c78aa5
JayCode Developer <JayCode.contakt@gmail.com>
2023-05-11 00:03:56 +0200

    Styled Arrow Button

--- src/components/arrowButton/arrowButton.styles.ts
+++ src/components/arrowButton/arrowButton.styles.ts
@@ -2,4 +2,50 @@ import styled from "styled-components";
 import Link from 'next/link';
 
 export const StyledOuterWrapper = styled(Link)`
+    display: inline-flex;
+    transition-property: color, fill, stroke;
+    transition: color 0.2s ease-in-out, transform 0.2s ease-in-out;
+    font-weight: 500;
+    text-align: left;
+    align-items: center;
+    text-decoration: none;
+    color: ${({ theme }) => theme.white};
+    transform: translateX(0);
+    margin-top: 2rem;
+    cursor: pointer;
+    span {
+        margin-right: 3.5rem;
+        font-size: ${({ theme }) => theme.font.size.s};
+        line-height: 2.5rem;
+        font-weight: 500;
+    }
+    &:hover{
+        color: ${({ theme }) => theme.grey};
+        transform: translateX(1rem);
+    }
+    ${({ theme }) => theme.mq.desktop} {
+        margin-top: 0rem;
+    }
+`
+
+export const StyledWrapper = styled.div`
+    display: inline-flex;
+    position: relative;
+    padding: 0.25rem;
+    flex: none;
+    justify-content: center;
+    align-items: center;
+    width: 3.5rem;
+    height: 3.5rem;
+    span {
+        transform: none;
+        margin: 0;
+    }
+    div {
+        position: absolute;
+    }
+`
+
+export const StyledArrow = styled.svg`
+    transform: rotate(-90deg);
 `
--- src/components/arrowButton/arrowButton.tsx
+++ src/components/arrowButton/arrowButton.tsx
@@ -1,5 +1,9 @@
 import React from "react";
-import { StyledOuterWrapper } from "./arrowButton.styles";
+import {
+  StyledOuterWrapper,
+  StyledWrapper,
+  StyledArrow,
+} from "./arrowButton.styles";
 
 export interface ArrowButtonProps {
   children: React.ReactNode;
@@ -7,7 +11,51 @@ export interface ArrowButtonProps {
 }
 
 const ArrowButton = ({ children, to }: ArrowButtonProps) => {
-  return <StyledOuterWrapper href={to}>{children}</StyledOuterWrapper>;
+  return (
+    <StyledOuterWrapper href={to}>
+      <span>{children}</span>
+      <StyledWrapper>
+        <div>
+          <svg width="60" height="60">
+            <circle
+              stroke="currentColor"
+              stroke-width="2"
+              fill="transparent"
+              r="28"
+              cx="30"
+              cy="30"
+            ></circle>
+            <circle
+              stroke="currentColor"
+              stroke-width="2"
+              fill="transparent"
+              r="28"
+              cx="30"
+              cy="30"
+              transform-origin="30px 30px"
+              stroke-dashoffset="175.92918860102841"
+            ></circle>
+          </svg>
+        </div>
+        <span>
+          <StyledArrow
+            width="32"
+            height="32"
+            viewBox="0 0 32 32"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M15.101 5.5V23.1094L9.40108 17.4095L8.14807 18.6619L15.9862 26.5L23.852 18.6342L22.5996 17.3817L16.8725 23.1094V5.5H15.101Z"
+              fill="currentColor"
+            ></path>
+          </StyledArrow>
+        </span>
+      </StyledWrapper>
+    </StyledOuterWrapper>
+  );
 };
 
 export default ArrowButton;
