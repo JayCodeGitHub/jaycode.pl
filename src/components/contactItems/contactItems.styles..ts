c503fe5d282a1fac95e9eed84b89ee7036078903
JayCode Developer <JayCode.contakt@gmail.com>
2023-05-10 15:12:43 +0200

    Styled contactItems

--- src/components/contactItems/contactItems.styles..ts
+++ src/components/contactItems/contactItems.styles..ts
@@ -1,3 +1,56 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
-export const StyledWrapper = styled.section``
+export const StyledOuterWrapper = styled.section`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    min-height: calc(65vh);
+    align-items: center;
+    flex-direction: column;
+    ${({ theme }) => theme.mq.desktop} {  
+        height: 55vh;
+        flex-direction: row;
+    }
+`
+
+
+export const StyledWrapper = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: space-around;
+    flex-direction: column;
+    align-items: center;
+    margin: 2rem 0;
+    ${({ theme }) => theme.mq.desktop} {        
+        width: 100%;
+        margin: 0;
+        flex-direction: row;
+        justify-content: space-around;
+    }
+`
+
+export const StyledContactItem = styled.a`
+    font-size: ${({ theme }) => theme.font.size.s};
+    text-decoration: none;
+    display: flex;
+    width: 90%;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 4rem 6rem;
+    border-radius: 2rem;
+    border: 2px solid ${({ theme }) => theme.theme};
+    margin: 2rem 0;
+    color: ${({ theme }) => theme.white};
+    transition: color 0.1s ease-in-out;
+    ${({ theme }) => theme.mq.desktop} {        
+        font-size: ${({ theme }) => theme.font.size.m};
+        width: auto;
+    }
+    &:hover {
+        color: ${({ theme }) => theme.grey};
+    }
+    svg {
+        margin: 0 2rem;
+    }
+`
--- src/components/contactItems/contactItems.tsx
+++ src/components/contactItems/contactItems.tsx
@@ -1,9 +1,34 @@
-import { StyledWrapper } from "./contactItems.styles.";
+import {
+  StyledOuterWrapper,
+  StyledWrapper,
+  StyledContactItem,
+} from "./contactItems.styles.";
+import { BsLinkedin, BsGithub } from "react-icons/bs";
+import { FiMail } from "react-icons/fi";
 
 export default function ContactItems() {
   return (
-    <StyledWrapper>
-      <h1>ContactItems</h1>
-    </StyledWrapper>
+    <StyledOuterWrapper>
+      <StyledWrapper>
+        <StyledContactItem
+          href="https://www.linkedin.com/in/jakub-gryzi%C5%84ski-b00296264/"
+          target="_blank"
+        >
+          <BsLinkedin />
+          LinkedIn
+        </StyledContactItem>
+        <StyledContactItem href="mailto:jakub.gryzinski@gmail.com">
+          <FiMail />
+          Email
+        </StyledContactItem>
+        <StyledContactItem
+          href="https://github.com/JayCodeGitHub"
+          target="_blank"
+        >
+          <BsGithub />
+          GitHub
+        </StyledContactItem>
+      </StyledWrapper>
+    </StyledOuterWrapper>
   );
 }
