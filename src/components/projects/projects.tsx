103a0b918e815cae42b0ddc3649ae631268cec3a
JayCode Developer <JayCode.contakt@gmail.com>
2023-05-12 23:08:09 +0200

    Created Projects

---
+++ src/components/projects/projects.tsx
@@ -0,0 +1,9 @@
+import { StyledWrapper } from "./projects.styles";
+
+export default function Projects() {
+  return (
+    <StyledWrapper>
+      <h1>Projects</h1>
+    </StyledWrapper>
+  );
+}
--- src/pages/experience.tsx
+++ src/pages/experience.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/header/header";
+import Projects from "@/components/projects/projects";
 
 export default function Experience() {
   return (
     <main>
       <Header title="Experience" />
+      <Projects />
     </main>
   );
 }
