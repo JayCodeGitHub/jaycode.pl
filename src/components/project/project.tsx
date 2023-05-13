b7f1afce450ca5c99c8c4282602003745dd9bc30
JayCode Developer <JayCode.contakt@gmail.com>
2023-05-13 19:53:00 +0200

    Refactored project

--- src/components/project/project.tsx
+++ src/components/project/project.tsx
@@ -11,33 +11,41 @@ import {
 } from "./project.styles";
 
 export interface ProjectProps {
-  i: number;
+  order: number;
+  image: string;
+  alt: string;
+  type: string;
+  title: string;
+  description: string;
+  technologies: Array<string>;
+  href: string;
+  github: string;
 }
 
-export default function Project({ i }: ProjectProps) {
+export default function Project({
+  order,
+  image,
+  alt,
+  type,
+  title,
+  description,
+  technologies,
+  href,
+  github,
+}: ProjectProps) {
   return (
     <StyledWrapper>
-      <StyledImageWrapper order={i}>
-        <StyledImage
-          src={"/images/karate-team.jpg"}
-          width={1500}
-          height={771}
-          alt="Project image"
-        />
+      <StyledImageWrapper order={order} href={href} target="_blank">
+        <StyledImage src={image} width={1500} height={771} alt={alt} />
       </StyledImageWrapper>
-      <StyledDescriptionsWrapper order={i}>
-        <StyledType>Commercial</StyledType>
-        <StyledTitle>Karate</StyledTitle>
-        <StyledDescription>
-          Still in progress refreshed website of Karate Team Oborniki.
-          Technologies used in the project were selected for the clients needs.
-          So I used Server Site Rendering to speed up loading times. For
-          animation I used GSAP and ScrollTrigger. The data is downloaded from
-          Dato CMS from where the client can manage the content of the site.
-        </StyledDescription>
+      <StyledDescriptionsWrapper order={order}>
+        <StyledType>{type}</StyledType>
+        <StyledTitle>{title}</StyledTitle>
+        <StyledDescription>{description}</StyledDescription>
         <StyledTechnologiesWrapper>
-          <StyledTechnology>React</StyledTechnology>
-          <StyledTechnology>Next.js</StyledTechnology>
+          {technologies.map((item, i) => (
+            <StyledTechnology key={i}>{item}</StyledTechnology>
+          ))}
         </StyledTechnologiesWrapper>
       </StyledDescriptionsWrapper>
     </StyledWrapper>
--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -1,12 +1,67 @@
 import { StyledWrapper } from "./projects.styles";
 import Project from "../project/project";
 
+const ProjectsItems = [
+  {
+    image: `/images/karate-team.jpg`,
+    alt: `Screenshot from Karate Team desktop website`,
+    type: `commercial`,
+    title: `Karate-Team`,
+    description: `Still in progress refreshed website of Karate Team Oborniki. Technologies used in the project were selected for the client's needs. So I used Server Site Rendering to speed up loading times. For animation I used GSAP and ScrollTrigger. The data is downloaded from Dato CMS from where the client can manage the content of the site.`,
+    technologies: [
+      "React",
+      "Gatsby",
+      "Dato CMS",
+      "styled-components",
+      "GraphQL",
+    ],
+    href: "https://karate-team.netlify.app/",
+    github: `https://github.com/JayCodeGitHub/karate-team.pl`,
+  },
+  {
+    image: ``,
+    alt: ``,
+    type: ``,
+    title: ``,
+    description: ``,
+    technologies: [],
+    href: "",
+    github: ``,
+  },
+  {
+    image: ``,
+    alt: ``,
+    type: ``,
+    title: ``,
+    description: ``,
+    technologies: [],
+    href: "",
+    github: ``,
+  },
+];
+
 export default function Projects() {
   return (
     <StyledWrapper>
-      <Project i={0} />
-      <Project i={1} />
-      <Project i={2} />
+      {ProjectsItems.map(
+        (
+          { image, alt, type, title, description, technologies, href, github },
+          i
+        ) => (
+          <Project
+            key={i}
+            order={i}
+            image={image}
+            alt={alt}
+            type={type}
+            title={title}
+            description={description}
+            technologies={technologies}
+            href={href}
+            github={github}
+          />
+        )
+      )}
     </StyledWrapper>
   );
 }
