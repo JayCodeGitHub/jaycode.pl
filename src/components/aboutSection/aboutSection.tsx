08be7cd1378b6a314b0af2aa946287dbed0ba2ee
JayCode Developer <JayCode.contakt@gmail.com>
2023-05-13 22:55:24 +0200

    Created about section

---
+++ src/components/aboutSection/aboutSection.tsx
@@ -0,0 +1,9 @@
+import { StyledWrapper } from "./aboutSection.styles";
+
+export default function AboutSection() {
+  return (
+    <StyledWrapper>
+      <h1>About</h1>
+    </StyledWrapper>
+  );
+}
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,11 @@
 import Header from "@/components/header/header";
+import AboutSection from "@/components/aboutSection/aboutSection";
 
 export default function About() {
   return (
     <main>
       <Header title="About" />
+      <AboutSection />
     </main>
   );
 }
