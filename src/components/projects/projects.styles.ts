62e96a69b06e8ae956b39f5dd99ea8f5885d5370
JayCode Developer <JayCode.contakt@gmail.com>
2023-05-13 12:19:33 +0200

    Styled projects section

--- src/components/projects/projects.styles.ts
+++ src/components/projects/projects.styles.ts
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 
 export const StyledWrapper = styled.section`
-    
+    padding: 2.5vw;
+    ${({ theme }) => theme.mq.bigDesktop} {
+        padding: 2.5vw 10vw;
+    }
 `
