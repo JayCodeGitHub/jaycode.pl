eb86376733e141604a77f5e5d62ec0c91faf9e6c
JayCode Developer <JayCode.contakt@gmail.com>
2023-05-13 12:55:10 +0200

    Create simple project structure

--- src/components/project/project.styles.ts
+++ src/components/project/project.styles.ts
@@ -1,5 +1,35 @@
 import styled from "styled-components";
+import Image from "next/image";
 
 export const StyledWrapper = styled.div`
-
+    display: flex;
+    align-items: center;
+    position: relative;
+    width: 100%;
+    margin-bottom: 150px;
+    gap: 10px;
 `
+
+export const StyledImageWrapper = styled.a`
+    width: 50%;
+    height: 100%;
+`
+
+export const StyledImage = styled(Image)`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: 1.2rem;
+`
+
+export const StyledDescriptionsWrapper = styled.div``
+
+export const StyledType = styled.p``
+
+export const StyledTitle = styled.h2``
+
+export const StyledDescription = styled.p``
+
+export const StyledTechnologiesWrapper = styled.ul``
+
+export const StyledTechnology = styled.li``
--- src/components/project/project.tsx
+++ src/components/project/project.tsx
@@ -1,9 +1,35 @@
-import { StyledWrapper } from "./project.styles";
+import {
+  StyledWrapper,
+  StyledImageWrapper,
+  StyledImage,
+  StyledDescriptionsWrapper,
+  StyledType,
+  StyledTitle,
+  StyledDescription,
+  StyledTechnologiesWrapper,
+  StyledTechnology,
+} from "./project.styles";
 
 export default function Project() {
   return (
     <StyledWrapper>
-      <h1>Project</h1>
+      <StyledImageWrapper>
+        <StyledImage
+          src={"/images/karate.webp"}
+          width={1590}
+          height={1000}
+          alt="Project image"
+        />
+      </StyledImageWrapper>
+      <StyledDescriptionsWrapper>
+        <StyledType>Commercial</StyledType>
+        <StyledTitle>Karate</StyledTitle>
+        <StyledDescription>Description</StyledDescription>
+        <StyledTechnologiesWrapper>
+          <StyledTechnology>React</StyledTechnology>
+          <StyledTechnology>Next.js</StyledTechnology>
+        </StyledTechnologiesWrapper>
+      </StyledDescriptionsWrapper>
     </StyledWrapper>
   );
 }
