c47e5dd62703edc639826143b1551f53967f39cb
JayCode Developer <JayCode.contakt@gmail.com>
2023-05-14 14:35:47 +0200

    Created about section structure

--- src/components/aboutSection/aboutSection.styles.ts
+++ src/components/aboutSection/aboutSection.styles.ts
@@ -1,3 +1,18 @@
 import styled from "styled-components";
+import Image from "next/image";
 
-export const StyledWrapper = styled.section``
+export const StyledOuterWrapper = styled.section``
+
+export const StyledWrapper = styled.div``
+
+export const StyledDescriptionsWrapper = styled.div``
+
+export const StyledTitle = styled.h2``
+
+export const StyledDescription = styled.p``
+
+export const StyledTechnologiesWrapper = styled.ul``
+
+export const StyledTechnology = styled.li``
+
+export const StyledImage = styled(Image)``
--- src/components/aboutSection/aboutSection.tsx
+++ src/components/aboutSection/aboutSection.tsx
@@ -1,9 +1,38 @@
-import { StyledWrapper } from "./aboutSection.styles";
+import {
+  StyledOuterWrapper,
+  StyledWrapper,
+  StyledDescriptionsWrapper,
+  StyledTitle,
+  StyledDescription,
+  StyledTechnologiesWrapper,
+  StyledTechnology,
+  StyledImage,
+} from "./aboutSection.styles";
 
 export default function AboutSection() {
   return (
-    <StyledWrapper>
-      <h1>About</h1>
-    </StyledWrapper>
+    <StyledOuterWrapper>
+      <StyledWrapper>
+        <StyledDescriptionsWrapper>
+          <StyledTitle>Hello!!! My name is Jakub</StyledTitle>
+          <StyledDescription>
+            I like to create things that live on the internet. For several years
+            I have been developing my skills in HTML, CSS, and JavaScript. I
+            will be a good member of the team because I learn quickly and work
+            well in a team.
+          </StyledDescription>
+          <StyledTechnologiesWrapper>
+            <StyledTechnology>React</StyledTechnology>
+          </StyledTechnologiesWrapper>
+        </StyledDescriptionsWrapper>
+        <StyledImage
+          src="/images/profile.webp"
+          loading="eager"
+          width={500}
+          height={643}
+          alt="Profile image"
+        />
+      </StyledWrapper>
+    </StyledOuterWrapper>
   );
 }
