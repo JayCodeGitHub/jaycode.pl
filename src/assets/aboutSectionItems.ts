a828cc29ddbb3ccec9116a7ec832616e6f511405
JayCode Developer <JayCode.contakt@gmail.com>
2023-05-14 17:20:40 +0200

    Moved about section items to separate file

---
+++ src/assets/aboutSectionItems.ts
@@ -0,0 +1,46 @@
+export const AboutSectionItems = {
+    title: `Hello!!! My name is Jakub`,
+    description: ` I like to create things that live on the internet. For several years
+    I have been developing my skills in HTML, CSS, and JavaScript. I
+    will be a good member of the team because I learn quickly and work
+    well in a team.`,
+    technologies: [
+      {
+        image: `/icons/scss.svg`,
+        alt: `SCSS logo`,
+      },
+      {
+        image: `/icons/html.svg`,
+        alt: `HTML logo`,
+      },
+      {
+        image: `/icons/js.svg`,
+        alt: `JavaScript logo`,
+      },
+      {
+        image: `/icons/ts.svg`,
+        alt: `TypeScript logo`,
+      },
+      {
+        image: `/icons/redux.svg`,
+        alt: `Redux logo`,
+      },
+      {
+        image: `/icons/React.svg`,
+        alt: `React logo`,
+      },
+      {
+        image: `/icons/tailwind.svg`,
+        alt: `Tailwind logo`,
+      },
+      {
+        image: `/icons/graphql.svg`,
+        alt: `GraphQL logo`,
+      },
+      {
+        image: `/icons/git.svg`,
+        alt: `Git logo`,
+      },
+    ],
+    image: `/images/profile.webp`,
+  };
--- src/components/aboutSection/aboutSection.tsx
+++ src/components/aboutSection/aboutSection.tsx
@@ -10,53 +10,7 @@ import {
   StyledImageWrapper,
   StyledImage,
 } from "./aboutSection.styles";
-
-const AboutSectionItems = {
-  title: `Hello!!! My name is Jakub`,
-  description: ` I like to create things that live on the internet. For several years
-  I have been developing my skills in HTML, CSS, and JavaScript. I
-  will be a good member of the team because I learn quickly and work
-  well in a team.`,
-  technologies: [
-    {
-      image: `/icons/scss.svg`,
-      alt: `SCSS logo`,
-    },
-    {
-      image: `/icons/html.svg`,
-      alt: `HTML logo`,
-    },
-    {
-      image: `/icons/js.svg`,
-      alt: `JavaScript logo`,
-    },
-    {
-      image: `/icons/ts.svg`,
-      alt: `TypeScript logo`,
-    },
-    {
-      image: `/icons/redux.svg`,
-      alt: `Redux logo`,
-    },
-    {
-      image: `/icons/React.svg`,
-      alt: `React logo`,
-    },
-    {
-      image: `/icons/tailwind.svg`,
-      alt: `Tailwind logo`,
-    },
-    {
-      image: `/icons/graphql.svg`,
-      alt: `GraphQL logo`,
-    },
-    {
-      image: `/icons/git.svg`,
-      alt: `Git logo`,
-    },
-  ],
-  image: `/images/profile.webp`,
-};
+import { AboutSectionItems } from "@/assets/aboutSectionItems";
 
 export default function AboutSection() {
   const { title, description, technologies, image } = AboutSectionItems;
