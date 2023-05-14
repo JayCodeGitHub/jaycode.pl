c0eaa13d73fa07ff7c32bced298ab206890f2a96
JayCode Developer <JayCode.contakt@gmail.com>
2023-05-14 17:18:58 +0200

    Refactored about Section

--- src/components/aboutSection/aboutSection.tsx
+++ src/components/aboutSection/aboutSection.tsx
@@ -11,59 +11,74 @@ import {
   StyledImage,
 } from "./aboutSection.styles";
 
+const AboutSectionItems = {
+  title: `Hello!!! My name is Jakub`,
+  description: ` I like to create things that live on the internet. For several years
+  I have been developing my skills in HTML, CSS, and JavaScript. I
+  will be a good member of the team because I learn quickly and work
+  well in a team.`,
+  technologies: [
+    {
+      image: `/icons/scss.svg`,
+      alt: `SCSS logo`,
+    },
+    {
+      image: `/icons/html.svg`,
+      alt: `HTML logo`,
+    },
+    {
+      image: `/icons/js.svg`,
+      alt: `JavaScript logo`,
+    },
+    {
+      image: `/icons/ts.svg`,
+      alt: `TypeScript logo`,
+    },
+    {
+      image: `/icons/redux.svg`,
+      alt: `Redux logo`,
+    },
+    {
+      image: `/icons/React.svg`,
+      alt: `React logo`,
+    },
+    {
+      image: `/icons/tailwind.svg`,
+      alt: `Tailwind logo`,
+    },
+    {
+      image: `/icons/graphql.svg`,
+      alt: `GraphQL logo`,
+    },
+    {
+      image: `/icons/git.svg`,
+      alt: `Git logo`,
+    },
+  ],
+  image: `/images/profile.webp`,
+};
+
 export default function AboutSection() {
+  const { title, description, technologies, image } = AboutSectionItems;
   return (
     <StyledOuterWrapper>
       <StyledWrapper>
         <StyledDescriptionsWrapper>
-          <StyledTitle>Hello!!! My name is Jakub</StyledTitle>
-          <StyledDescription>
-            I like to create things that live on the internet. For several years
-            I have been developing my skills in HTML, CSS, and JavaScript. I
-            will be a good member of the team because I learn quickly and work
-            well in a team.
-          </StyledDescription>
+          <StyledTitle>{title}</StyledTitle>
+          <StyledDescription>{description}</StyledDescription>
           <StyledLinksWrapper>Linked In, GitHub</StyledLinksWrapper>
           <StyledTechnologiesWrapper>
             <h3>Tech Stack</h3>
-            <li>
-              <StyledTechnology src="/icons/scss.svg" alt="Technology Logo" />
-            </li>
-            <li>
-              <StyledTechnology src="/icons/html.svg" alt="Technology Logo" />
-            </li>
-            <li>
-              <StyledTechnology src="/icons/js.svg" alt="Technology Logo" />
-            </li>
-            <li>
-              <StyledTechnology src="/icons/ts.svg" alt="Technology Logo" />
-            </li>
-            <li>
-              <StyledTechnology src="/icons/redux.svg" alt="Technology Logo" />
-            </li>
-            <li>
-              <StyledTechnology src="/icons/react.svg" alt="Technology Logo" />
-            </li>
-            <li>
-              <StyledTechnology
-                src="/icons/tailwind.svg"
-                alt="Technology Logo"
-              />
-            </li>
-            <li>
-              <StyledTechnology
-                src="/icons/graphql.svg"
-                alt="Technology Logo"
-              />
-            </li>
-            <li>
-              <StyledTechnology src="/icons/git.svg" alt="Technology Logo" />
-            </li>
+            {technologies.map(({ image, alt }) => (
+              <li key="alt">
+                <StyledTechnology src={image} alt={alt} />
+              </li>
+            ))}
           </StyledTechnologiesWrapper>
         </StyledDescriptionsWrapper>
         <StyledImageWrapper>
           <StyledImage
-            src="/images/profile.webp"
+            src={image}
             loading="eager"
             width={500}
             height={643}
