901dfc46b2abd83a4e44f364d9d1727e0551d0f9
JayCode Developer <JayCode.contakt@gmail.com>
2023-05-14 20:04:37 +0200

    Created Social Media Button

---
+++ src/components/socialMediaButton/socialMediaButton.tsx
@@ -0,0 +1,9 @@
+import { StyledWrapper } from "./socialMediaButton.styled";
+
+export default function socialMediaButton() {
+  return (
+    <StyledWrapper>
+      <h1>SocialMedia Link</h1>
+    </StyledWrapper>
+  );
+}
