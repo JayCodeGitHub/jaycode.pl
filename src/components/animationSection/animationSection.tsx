1ef79326153f3c7bb402d5cf1da23241fa12e3f2
JayCode Developer <JayCode.contakt@gmail.com>
2023-05-16 00:29:11 +0200

    Created animation section

---
+++ src/components/animationSection/animationSection.tsx
@@ -0,0 +1,9 @@
+import { StyledWrapper } from "./animationSection.styles";
+
+export default function AnimationSection() {
+  return (
+    <StyledWrapper>
+      <h1>Animation Section</h1>
+    </StyledWrapper>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/hero/hero";
 import Tiles from "@/components/tiles/tiles";
+import AnimationSection from "@/components/animationSection/animationSection";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <Tiles />
+      <AnimationSection />
     </main>
   );
 }
