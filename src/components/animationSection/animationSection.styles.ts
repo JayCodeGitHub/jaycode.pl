dbc75cd06ad7c5726d281cc8f7147f579945fce6
JayCode Developer <JayCode.contakt@gmail.com>
2023-05-17 17:44:46 +0200

    Styled animation section

--- src/components/animationSection/animationSection.styles.ts
+++ src/components/animationSection/animationSection.styles.ts
@@ -1,3 +1,57 @@
 import styled from "styled-components";
 
-export const StyledWrapper = styled.section``
+export const StyledWrapper = styled.section`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    margin: 5rem 0;
+    padding: 2.5vw;
+    ${({ theme }) => theme.mq.desktop} {
+        flex-direction: row;
+        padding: 0 15vw;
+        margin: 2.5vw 0 4vw 0;
+    }
+`
+
+export const StyledSvgWrapper = styled.div`
+    width: 100%;
+    svg {
+        width: 100%;
+        height: auto;
+    }
+    ${({ theme }) => theme.mq.desktop} {
+        width: 50%;
+    }
+`
+
+export const StyledDescriptions = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: space-between;
+    width: 100%;
+    ${({ theme }) => theme.mq.desktop} {
+        width: 50%;
+    }
+`
+
+export const StyledTitle = styled.h1`
+    margin: 2.5rem 0;
+    width: 100%;
+    font-size: ${({ theme }) => theme.font.size.s};
+    color: ${({ theme }) => theme.white};
+    ${({ theme }) => theme.mq.desktop} {
+        font-size: ${({ theme }) => theme.font.size.m};
+    }
+`
+
+export const StyledDescription = styled.p`
+    color: ${({ theme }) => theme.grey};
+    width: 100%;
+    font-size: ${({ theme }) => theme.font.size.xs};
+    ${({ theme }) => theme.mq.desktop} {
+        font-size: ${({ theme }) => theme.font.size.s};
+    }
+`
--- src/components/animationSection/animationSection.tsx
+++ src/components/animationSection/animationSection.tsx
@@ -1,9 +1,116 @@
-import { StyledWrapper } from "./animationSection.styles";
+import { useRef, useEffect } from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
+import {
+  StyledWrapper,
+  StyledSvgWrapper,
+  StyledDescriptions,
+  StyledTitle,
+  StyledDescription,
+} from "./animationSection.styles";
+import Scene from "./scene";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function AnimationSection() {
+  const wrapper = useRef<any>(null);
+  useEffect(() => {
+    if (wrapper.current) {
+      const [elements] = wrapper.current.children;
+
+      const background = elements.getElementById("background");
+      const desktop = elements.getElementById("desktop");
+      const laptop = elements.getElementById("laptop");
+      const cup = elements.getElementById("cup");
+      const character = elements.getElementById("character");
+
+      gsap.set([background, desktop, laptop, cup, character], {
+        autoAlpha: 0,
+      });
+
+      gsap.fromTo(
+        background,
+        {},
+        {
+          autoAlpha: 1,
+          duration: 1,
+          scrollTrigger: {
+            trigger: wrapper.current,
+            start: "20% 50%",
+          },
+        }
+      );
+      gsap.fromTo(
+        desktop,
+        { y: "-=100" },
+        {
+          y: 0,
+          autoAlpha: 1,
+          duration: 1,
+          delay: 0.5,
+          scrollTrigger: {
+            trigger: wrapper.current,
+            start: "20% 50%",
+          },
+        }
+      );
+      gsap.fromTo(
+        laptop,
+        { y: "-=100" },
+        {
+          y: 0,
+          autoAlpha: 1,
+          duration: 1,
+          delay: 1,
+          scrollTrigger: {
+            trigger: wrapper.current,
+            start: "20% 50%",
+          },
+        }
+      );
+      gsap.fromTo(
+        cup,
+        { x: "+=100" },
+        {
+          x: 0,
+          autoAlpha: 1,
+          duration: 1,
+          delay: 1.5,
+          scrollTrigger: {
+            trigger: wrapper.current,
+            start: "20% 50%",
+          },
+        }
+      );
+      gsap.fromTo(
+        character,
+        {},
+        {
+          autoAlpha: 1,
+          duration: 1,
+          delay: 2,
+          scrollTrigger: {
+            trigger: wrapper.current,
+            start: "20% 50%",
+          },
+        }
+      );
+    }
+  });
   return (
     <StyledWrapper>
-      <h1>Animation Section</h1>
+      <StyledSvgWrapper ref={wrapper}>
+        <Scene />
+      </StyledSvgWrapper>
+      <StyledDescriptions>
+        <StyledTitle>
+          The applications I create are easy to use and easy to develop.
+        </StyledTitle>
+        <StyledDescription>
+          I have experience in writing applications in JavaScript ES6+ with use
+          async and await. And experience with Git and GitHub
+        </StyledDescription>
+      </StyledDescriptions>
     </StyledWrapper>
   );
 }
