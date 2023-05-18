2856a9b45f9350099e3f79ffb125837684e9845e
JayCode Developer <JayCode.contakt@gmail.com>
2023-05-19 00:27:31 +0200

    Fix social media button mobile view

--- src/components/socialMediaButton/socialMediaButton.styled.ts
+++ src/components/socialMediaButton/socialMediaButton.styled.ts
@@ -16,7 +16,7 @@ export const StyledWrapper = styled.button`
     background-color: ${({ theme }) => theme.tertiary};
     border: solid 2px ${({ theme }) => theme.white};
     border-radius: 1.2rem;
-    padding: .8rem 2rem;
+    padding: .6rem 1.5rem;
     width: 100%;
     transition: all 0.2s ease-in-out;
     svg {
@@ -25,5 +25,9 @@ export const StyledWrapper = styled.button`
     &:hover {
         color: ${({ theme }) => theme.grey};
         border: solid 2px ${({ theme }) => theme.grey};
+    }
+    ${({ theme }) => theme.mq.desktop}  {
+        font-size: ${({ theme }) => theme.font.size.xs};
+        padding: .8rem 2rem;
     }
 `
